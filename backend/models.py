7b59085bc508245860a20cc4a1b2244fb8345132
Steve132 <steve@soapforge.com>
2015-09-26 03:23:19 -0400

    Models

---
+++ backend/models.py
@@ -0,0 +1,13 @@
+from google.appengine.ext import ndb
+class PermissionsModel(ndb.Model):
+	read=ndb.KeyProperty(indexed=True,repeated=True,required=True)
+	edit=ndb.KeyProperty(indexed=True,repeated=True,required=True)
+	mark=ndb.KeyProperty(indexed=True,repeated=True,required=True)
+	assign=ndb.KeyProperty(indexed=True,repeated=True,required=True)
+
+class Task(ndb.Model):
+	permissions=ndb.StructuredProperty(PermissionsModel,indexed=True)
+	type=ndb.StringProperty(indexed=True)
+	client_data=ndb.JsonProperty(indexed=False)
+	customers=ndb.KeyProperty(Task,indexed=True)
+	last_updated=ndb.DateTimeProperty(indexed=True,auto_now=True)
